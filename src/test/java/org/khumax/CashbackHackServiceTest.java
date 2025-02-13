81c5226f82e7f70d78cf9ef9a6df72d9a713d8b9
Khumax <m.khudoteplov@gmail.com>
2025-02-13 12:33:46 +0300

    add junit4

--- src/test/java/org/khumax/CashbackHackServiceTest.java
+++ src/test/java/org/khumax/CashbackHackServiceTest.java
@@ -1,57 +1,57 @@
 package org.khumax;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldSuggestAddingGoodsIfLessThanBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldSuggestAddingGoodsIfOneUnderBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
 
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldNotSuggestAddingGoodsIfEqualToBoundary() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 999;
+        int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 1;
-        assertEquals(actual, expected);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldNotSuggestAddingGoodsIfOneUpperBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
 
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldNotSuggestAddingGoodsIfMoreThanBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1201;
 
         int actual = service.remain(amount);
         int expected = 799;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
