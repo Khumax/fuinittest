ecae953c6c885d4bc59dc871c5cf78470625fe44
Khumax <m.khudoteplov@gmail.com>
2025-02-16 08:20:54 +0300

    Update CashbackHackServiceTest.java for CI fail!!!

--- src/test/java/org/khumax/CashbackHackServiceTest.java
+++ src/test/java/org/khumax/CashbackHackServiceTest.java
@@ -27,10 +27,10 @@ public class CashbackHackServiceTest {
     @org.testng.annotations.Test
     public void shouldNotSuggestAddingGoodsIfEqualToBoundary() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 999;
+        int amount = 1000;
 
         int actual = service.remain(amount);
-        int expected = 1;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 
